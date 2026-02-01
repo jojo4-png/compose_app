5d57de86c614feb75133d4e81b0803f109a0c13d
Serjojo <yoosifkng4@gmail.com>
2026-02-01 14:07:02 -0800

    Initial commit with CI/CD

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+# 'redis' is the name of the other container we will create
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    return f'''
+    <div style="text-align:center; margin-top:100px; font-family:sans-serif;">
+        <h1 style="color: #FF5733;">🚀 Serjojo's Traffic Monitor</h1>
+        <p style="font-size: 24px;">This page has been viewed <b>{count}</b> times.</p>
+        <p style="color: gray;">Database: Redis | Framework: Flask</p>
+    </div>
+    '''
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
